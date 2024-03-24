d72e2d7334c52ccababeaebaab4006d21bb37955
AtinadCodes <lamptey.danita@gmail.com>
2024-03-24 21:42:40 +0000

    updates to form

--- src/pages/add-recipe/index.jsx
+++ src/pages/add-recipe/index.jsx
@@ -1,30 +1,62 @@
+// Forms.js
 import * as React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-
+import MenuItem from "@mui/material/MenuItem";
+import FileUpload from "../../components/fileupload";
+import "./form.css"; 
 
 const countries = [
-    'Ghana',
-    'France',
-    'Nigeria',
-    
-  ];
-  
+  {
+    value: "CH",
+    label: "China",
+  },
+  {
+    value: "FR",
+    label: "France",
+  },
+  {
+    value: "GH",
+    label: "Ghana",
+  },
+  {
+    value: "NG",
+    label: "Nigeria",
+  },
+];
 
-export default function Forms() {
+export default function Form() {
   return (
     <Box
+    sx={{
+      "& .MuiTextField-root": { m: 1, width: "25ch" },
+     
+    }}
+    noValidate
       component="form"
-      sx={{
-        "& .MuiTextField-root": { m: 1, width: "25ch" },
-      }}
-      noValidate
+      className="form-container" 
+      
+
     >
       <div>
-        <TextField required id="outlined-title" label="Title" />
-
-        <TextField id="outlinetd-description" label="Description" />
-        <TextField id="outlined- country" label="Country" />
+        <TextField required id="outlined-title" label="Title" className="form-field" /> <br />
+        <TextField id="outlinetd-description" label="Description" className="form-field" /> <br />
+        <TextField
+          id="outlined-select-country"
+          select
+          label="Select"
+          defaultValue="Ghana"
+          helperText="Please select your country"
+          className="form-field"
+        >
+          {countries.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+        <br />
+        <FileUpload className="form-field" />
       </div>
     </Box>
   );
